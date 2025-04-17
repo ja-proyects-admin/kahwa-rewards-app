d902eaee74d9a552ea768aeb8cd99a79f6814b00
ja-proyects-admin <julian.acosta.proyects@gmail.com>
2025-04-17 16:11:17 -0300

    nuevo push despues de habilitar cloud resource manager API

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Request
 
 @functions_framework.http
 def hello_http(request: Request):
-    name = request.args.get("name") or (request.get_json(silent=True) or {}).get("name", "Julián")
+    name = request.args.get("name") or (request.get_json(silent=True) or {}).get("name", "Julian")
     timestamp = datetime.utcnow().isoformat()
 
     message = f"Hola {name}, bienvenido a Kahwa Rewards!"
